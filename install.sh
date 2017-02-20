64a1f1e6f29f737345aeefd324beda144d0e5d7a
Harish.K <harish2704@gmail.com>
2017-02-20 11:52:17 +0530

    Fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 desktop-file-install --dir=$HOME/.local/share/applications --rebuild-mime-info-cache Alacritty.desktop 
 mkdir -p ~/.config/alacritty/
-ln -s $PWD/alacritty.yml ~/.config/alacritty/
+cp $PWD/alacritty.yml ~/.config/alacritty/
 
